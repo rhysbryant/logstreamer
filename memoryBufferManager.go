3eca62c8754171f2b6518168c9e7bef5c891277b
rhys <rhys@voltprojects.com>
2021-05-29 17:32:05 +1000

    add copyright

--- memoryBufferManager.go
+++ memoryBufferManager.go
@@ -1,5 +1,19 @@
 package main
 
+/**
+	This file is part of logstreamer.
+	logstreamer - printer status page and protocol relay for daVinci jr 3d printers
+    logstreamer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    logstreamer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with logstreamer.  If not, see <http://www.gnu.org/licenses/>.
+**/
 import (
 	"errors"
 	"sync"
